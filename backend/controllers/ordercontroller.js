1869c9f0d6cc4056867abc1c9b29f52731ab2bc8
Jagadees321 <yeddulajagadeesh11@gmail.com>
2025-05-07 15:29:35 +0530

    final backend

--- backend/controllers/ordercontroller.js
+++ backend/controllers/ordercontroller.js
@@ -39,4 +39,79 @@ async function placeorder(req,res){
     }
 }
 
-module.exports={placeorder }
+async function getorders(req,res){
+    try {
+        const userid=req.params.userid;
+        // Check if the user exists
+        const user = await userModel.findById(userid);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        //check user is admin or not
+        if(!user.isAdmin){
+            return res.status(403).json({ message: 'You are not authorized to view this page' });
+        }
+        const orders = await orderModel.find({}).populate('userid').populate('productids.productid');
+        if (!orders || orders.length === 0) {
+            return res.status(404).json({ message: 'No orders found' });
+        }
+       
+        return res.status(200).json({message:'Orders fetched successfully',orders});
+    } catch (error) {
+        console.log('Error fetching orders:', error);
+        return res.status(500).json({ message: 'Internal server error', error });
+    }
+}
+
+async function userorderhistory(req,res){
+    try {
+        const userid=req.params.userid;
+        // Check if the user exists
+        const user = await userModel.findById(userid);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const orders = await orderModel.find({userid}).populate('userid').populate('productids.productid');
+        if (!orders || orders.length === 0) {
+            return res.status(404).json({ message: 'No orders found' });
+        }
+       
+        
+        return res.status(200).json({message:'Orders fetched successfully',total:orders.length,noofproducts:orders[0].productids.length,orders});
+    } catch (error) {
+        console.log('Error fetching orders:', error);
+        return res.status(500).json({ message: 'Internal server error', error });
+    }
+}
+async function deleteorder(req,res){
+    try {
+        const orderid=req.params.id;
+        // Check if the order exists
+        const order = await orderModel.findById(orderid);
+        if (!order) {
+            return res.status(404).json({ message: 'Order not found' });
+        }
+        await orderModel.findByIdAndDelete(orderid);
+        return res.status(200).json({ message: 'Order deleted successfully' });
+    } catch (error) {
+        console.log('Error deleting order:', error);
+        return res.status(500).json({ message: 'Internal server error', error });
+    }
+}
+
+async function updateorder(req,res){
+    try {
+        const orderid=req.params.id;
+        // Check if the order exists
+        const order = await orderModel.findById(orderid);
+        if (!order) {
+            return res.status(404).json({ message: 'Order not found' });
+        }
+        const updatedOrder = await orderModel.findByIdAndUpdate(orderid, req.body, { new: true });
+        return res.status(200).json({ message: 'Order updated successfully', updatedOrder });
+    } catch (error) {
+        console.log('Error updating order:', error);
+        return res.status(500).json({ message: 'Internal server error', error });
+    }
+}
+module.exports={placeorder,getorders,userorderhistory ,updateorder,deleteorder};
--- backend/router/orderroute.js
+++ backend/router/orderroute.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { placeorder } = require('../controllers/ordercontroller');
+const { placeorder,getorders, userorderhistory ,deleteorder,updateorder} = require('../controllers/ordercontroller');
 
 
 
 // Route to place an order  
-router.post('/order/:userid',placeorder);
+router.post('/orders/:userid',placeorder);
+router.get('/orders/:userid',getorders);
+router.get('/orders/userorderhistory/:userid',userorderhistory);
+router.delete('/orders/:id',deleteorder);
+router.put('/orders/:id',updateorder);
 module.exports = router;
